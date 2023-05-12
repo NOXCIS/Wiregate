bf7870afc25b82b7c0d1e84c5b837dd49b0a15df
NOXCIS <shamarakeillee@gmail.com>
2023-05-12 14:12:26 -0500

    improve setup

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ function update_server_ip() {
   local yml_file="docker-compose.yml"
   local ip
 
-  read -p "Do you want to automatically get the server IP address? (Y/n) " auto_ip
+  read -p "Do you want to automatically get the server IP address? $(tput setaf 1)(y/n)$(tput sgr0) " auto_ip
   echo ""
   if [[ $auto_ip =~ ^[Nn]$ ]]; then
-    read -p "Please enter the server IP address (Press enter for default: 127.0.0.1 ): " ip
+    read -p "Please enter the server IP address $(tput setaf 1)(Press enter for default: $(tput sgr0)$(tput setaf 3)127.0.0.1$(tput sgr0)$(tput setaf 1)): $(tput sgr0) " ip
     ip=${ip:-127.0.0.1}
     echo ""
   else
@@ -17,7 +17,7 @@ function update_server_ip() {
 
   if [[ -f "$yml_file" ]]; then
     sed -i "s/SERVER_IP=.*/SERVER_IP=$ip/" "$yml_file"
-    echo "SERVER_IP parameter in $yml_file has been updated to $ip"
+    echo -e "Server IP address has been set to \033[32m$ip\033[0m"
     echo ""
   else
     echo "$yml_file not found."
@@ -28,11 +28,12 @@ function config_count() {
   local yml_file="docker-compose.yml"
 
   if [[ -f "$yml_file" ]]; then
-    read -p "Enter # of WireGuard server configurations to generate (Press enter for default: 1 ): " count
+                                                                   
+    read -p "Enter # of WireGuard server configurations to generate  $(tput setaf 1)(Press enter for default: $(tput sgr0)$(tput setaf 3)1$(tput sgr0)$(tput setaf 1)): $(tput sgr0) " count
     count=${count:-1} # set count to 1 if user enters nothing
     sed -i "s/CONFIG_CT=.*/CONFIG_CT=$count/" "$yml_file"
     echo ""
-    echo "WireGuard Server Configurations to be Generated has been set to $count"
+    echo -e "WireGuard Server Configurations to be Generated has been set to \033[32m$count\033[0m"
     echo ""
   else
     echo "$yml_file not found."
@@ -44,19 +45,28 @@ function config_count() {
 
 function set_password_and_tz {
     local yml_file="docker-compose.yml"
-    read -p "Do you want to automatically get the host timezone? (y/n) " answer
+    read -p "Do you want to automatically get the host timezone? $(tput setaf 1)(y/n)$(tput sgr0) " answer
     echo ""
     if [[ $answer == [Yy] ]]; then
         timezone=$(cat /etc/timezone)
+        echo -e "Timezone has been set to \033[32m$timezone\033[0m"
+        echo ""
     else
-        read -p "Enter timezone (Press enter for default: America/New_York ): " timezone
+        #read -p "Enter timezone ($(tput setaf 1)Press enter for default: America/New_York$(tput sgr0)): " timezone
+        read -p "Enter timezone $(tput setaf 1)(Press enter for default: $(tput sgr0)$(tput setaf 3)America/New_York$(tput sgr0)$(tput setaf 1)): $(tput sgr0) " timezone
+
+        echo ""
         timezone=${timezone:-"America/New_York"} # set timezone to America/New_York if user enters nothing
+        echo -e "Timezone has been set to \033[32m$timezone\033[0m"
         echo ""
     fi
     sed -i "s|TZ:.*|TZ: \"$timezone\"|" "$yml_file"
-    read -sp "Enter password for Pihole Dashboard (Press enter for default: No Password ): " password
+    read -sp "Enter password for Pihole Dashboard $(tput setaf 1)(Press enter for default: $(tput sgr0)$(tput setaf 3)No Password$(tput sgr0)$(tput setaf 1)): $(tput sgr0) " password && echo -e "\n"
     sed -i "s/WEBPASSWORD:.*/WEBPASSWORD: \"$password\"/" "$yml_file"
+    echo -e "\033[32mPASSWORD HAS BEEN SET\033[0m"
     echo ""
+
+
 }
 
 
@@ -73,59 +83,60 @@ PREREQUISITES=(
     software-properties-common
 )
 
+# Define ANSI color codes
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
+
 # Check if each prerequisite is already installed
 for prerequisite in "${PREREQUISITES[@]}"
 do
     if ! dpkg -s "$prerequisite" > /dev/null 2>&1; then
-        echo "$prerequisite is not installed. Installing..."
+        echo "${GREEN}$prerequisite is not installed. Installing...${RESET}"
         sudo apt-get update &&
         sudo apt-get install -yqq "$prerequisite"
     else
-        echo "$prerequisite is already installed. Skipping..."
+        echo "${GREEN}$prerequisite is already installed. Skipping...${RESET}"
     fi
 done
 
 # Check if docker-compose is already installed
 if ! command -v docker-compose > /dev/null 2>&1; then
-    echo "docker-compose is not installed. Installing..."
+    echo "${GREEN}docker-compose is not installed. Installing...${RESET}"
     # Install docker-compose
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
     sudo chmod +x /usr/local/bin/docker-compose &&
     sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 else
-    echo "docker-compose is already installed. Skipping..."
+    echo "${GREEN}docker-compose is already installed. Skipping...${RESET}"
 fi
 
-echo ""
-echo ""  
+echo -e "\033[33m\n"
 echo "#######################################################################"
-echo ""
 echo ""
 echo "                        INSTALLING PORTAINER"
 echo "                        RUNNING ON PORT :9000"
 echo ""
-echo ""
 echo "#######################################################################"
-echo ""
-echo ""  
+echo -e "\n\033[0m"
 sleep 1s
 
    #docker rm -f portainer
 if docker ps -a | grep portainer ; then
     echo "" 
-    echo "Portainer is already running"
+    echo -e "\033[32mPortainer is already running\033[0m"
 else
-    echo "Portainer is not running, creating data volume and starting container"
-    docker volume create portainer_data
-    docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+    echo -e "\033[31mPortainer is not running, creating data volume and starting container\033[0m" "\n"
+    echo -e "\033[32m$(docker volume create portainer_data)\033[0m" "\n"
+    echo -e "\033[32m$(docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce)\033[0m" "\n"
 fi
-
-sleep 2s
-clear
-
-
 echo ""
 echo ""  
+        sleep 2s
+        clear
+
+
+
+echo -e "\033[33m\n"
 echo "#######################################################################"
 echo ""
 echo ""
@@ -133,16 +144,16 @@ echo "                 SET TIMEZONE AND DASHBOARD PASSWORD"
 echo ""
 echo ""
 echo "#######################################################################"
-echo ""
-echo ""  
-sleep 1s
+echo -e "\n\033[0m"
+sleep 0.5s
 
-    set_password_and_tz &&
-        
+set_password_and_tz &&
         sleep 2s
         clear
-echo ""
-echo ""  
+
+
+
+echo -e "\033[33m\n" 
 echo "#######################################################################"
 echo ""
 echo ""
@@ -150,17 +161,17 @@ echo "             SETTING SERVER IP & SERVER CONFIG FOR WIREGUARD"
 echo ""
 echo ""
 echo "#######################################################################"
-echo ""
-echo ""  
-sleep 1s
-      update_server_ip &&
-          sleep 1s
+echo -e "\n\033[0m"
+sleep 0.5s
 
-      config_count &&
-          sleep 2s
-          clear
-echo ""
-echo ""      
+update_server_ip &&
+config_count &&
+        sleep 2s
+        clear
+
+
+
+echo -e "\033[33m\n"   
 echo "#######################################################################"
 echo ""
 echo ""
@@ -168,11 +179,10 @@ echo "                        REVIEW COMPOSE FILE"
 echo ""
 echo ""
 echo "#######################################################################"
-echo ""
-echo ""  
+echo -e "\n\033[0m"
 sleep 1s
 
-      nano docker-compose.yml
-      docker-compose up -d --build  &&
+nano docker-compose.yml
+docker-compose up -d --build  &&
 
 sleep 1s
