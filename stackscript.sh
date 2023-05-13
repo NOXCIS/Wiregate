91582c3c95cf6d5ff78d5453a382678220937e04
NOXCIS <shamarakeillee@gmail.com>
2023-05-13 18:53:26 -0500

    Update stackscript.sh

--- stackscript.sh
+++ stackscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-
+sudo sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qy update
 sudo sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qy upgrade
 #sudo apt-get update -q && sudo apt-get upgrade -qy
 git clone https://github.com/NOXCIS/Worm-Hole.git
