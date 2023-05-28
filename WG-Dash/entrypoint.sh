7a730f19a663988395bdb5c3815bcd979df8bc49
NOXCIS <shamarakeillee@gmail.com>
2023-05-28 13:02:42 -0500

    Update entrypoint.sh

--- WG-Dash/entrypoint.sh
+++ WG-Dash/entrypoint.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-rm /etc/wireguard/*.conf
+if [ -z "$(ls -A /etc/wireguard/*.conf 2>/dev/null)" ]; then
+  echo "No WireGuard configuration files found. Skipping removal."
+else
+  rm /etc/wireguard/*.conf
+  echo "Existing WireGuard configuration files removed."
+fi
 
 chmod u+x /home/app/wgd.sh
 
-if [ ! -f "/home/app/wg-dashboard.ini" ]; 
-then
+if [ ! -f "/home/app/wg-dashboard.ini" ]; then
   /home/app/wgd.sh install
-fi      
-
+fi
 
 if [ ! -f "/etc/wireguard/wg0.conf" ]; then
-for ((i=1; i<=$CONFIG_CT; i++))
-do
-  
+  for ((i=1; i<=$CONFIG_CT; i++))
+  do
     /home/app/wgd.sh newconfig
-  
-done
+  done
 fi
+
 /home/app/wgd.sh debug
