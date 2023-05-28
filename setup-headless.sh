6e0a61e3e7182a6d67e3300ff7dbf48edc463e48
NOXCIS <shamarakeillee@gmail.com>
2023-05-28 16:25:58 -0500

    Update setup-headless.sh

--- setup-headless.sh
+++ setup-headless.sh
@@ -346,7 +346,7 @@ echo "#######################################################################"
 echo -e "\n\033[0m"
 sleep 0.1s
           install_prerequisites &&
-
+sleep 3s
 
 echo -e "\033[33m\n" 
 echo "#######################################################################"
@@ -356,7 +356,22 @@ echo ""
 echo "#######################################################################"
 echo -e "\n\033[0m"
 sleep 0.1s
-          docker_compose_install &&
+          # Check if docker-compose is already installed
+            if ! command -v docker > /dev/null 2>&1; then
+                echo "${GREEN}docker is not installed. Installing...${RESET}"
+        # Install docker-compose
+                sudo install -m 0755 -d /etc/apt/keyrings
+                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+                sudo chmod a+r /etc/apt/keyrings/docker.gpg
+                echo \
+                "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+                "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                sudo apt-get update
+                sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qy install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+            else
+                echo "${GREEN}docker is already installed. Skipping...${RESET}"
+            fi
 sleep 5s
 
 
