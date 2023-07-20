2b7d1103df654abd8d8755a79f5c1164356b3913
NOXCIS <shamarakeillee@gmail.com>
2023-07-19 20:19:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -275,14 +275,18 @@ install_docker() {
 
     for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
     sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sleep 0.25s
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sleep 0.25s
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-    echo \
+    sleep 1s0.25s   echo \
     "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sleep 0.25s
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sleep 0.25s
     sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qy update > /dev/null 2>&1
+    sleep 0.25s
     sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qy install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
 
 }
