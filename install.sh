8736bf1cf0694cf2b846a7215ee21235dfc5ffb9
NOXCIS <shamarakeillee@gmail.com>
2023-07-19 20:12:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -251,8 +251,6 @@ install_prerequisites() {
         gnupg-agent
         software-properties-common
         openssl
-        docker
-        docker-compose
     )
     # Define ANSI color codes
     GREEN=$(tput setaf 2)
@@ -275,7 +273,7 @@ install_prerequisites() {
 }
 install_docker() {
 
-    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
     sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
