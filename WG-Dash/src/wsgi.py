4abb8a1564c970742459c3fefd59cf825727be4b
NOXCIS <shamarakeillee@gmail.com>
2023-10-29 01:26:54 -0500

    Update wsgi.py

--- WG-Dash/src/wsgi.py
+++ WG-Dash/src/wsgi.py
@@ -1,13 +1,5 @@
 from dashboard import app
-from dashboard import init_dashboard
-from dashboard import run_dashboard
-import configparser
 
-
-
-        
 
 if __name__ == "__main__":
-        run_dashboard()
-        init_dashboard()
         app.run()
