8b3d025c99a078758ab0fe89ea6ed532bf15c053
NOXCIS <shamarakeillee@gmail.com>
2023-11-29 08:31:44 -0600

    close

--- WG-Dash/src/postup.sh
+++ WG-Dash/src/postup.sh
@@ -13,7 +13,7 @@ iptables -N $CHAIN_NAME
 iptables -A FORWARD -j $CHAIN_NAME
 
 # Accept related or established traffic
-iptables -A $CHAIN_NAME -o $WIREGUARD_INTERFACE -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A $CHAIN_NAME -s 10.0.0.1/24 -o $WIREGUARD_INTERFACE -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 # Accept traffic from any Wireguard IP address connected to the Wireguard server
 iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -j ACCEPT
@@ -21,12 +21,23 @@ iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -j ACCEPT
 # Drop everything else coming through the Wireguard interface
 #iptables -A $CHAIN_NAME -i $WIREGUARD_INTERFACE -j DROP
 
-# DNS
-iptables -A $CHAIN_NAME -s 10.0.1.1/24 -i wg1 -d 10.2.0.3 -p udp --dport 53 -j ACCEPT
-# Drop traffic to your any private IP address
-iptables -A $CHAIN_NAME -s 10.0.1.1/24 -i wg1 -d 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 -j DROP
-# Accept outgoing connections to HTTP(S) ports to any IP address (public because of rule above)
-iptables -A $CHAIN_NAME -s 10.0.1.1/24 -i wg1 -d 0.0.0.0/0 -p tcp -m multiport --dports 80,443 -j ACCEPT
 
+
+# DNS
+# Accept DNS traffic from your Wireguard client
+iptables -A $CHAIN_NAME -s 10.2.0.100 -i wg1 -d 10.0.1.1/24 -o eth0 -p udp --dport 53 -j ACCEPT
+# Accept DNS traffic from your Wireguard server
+iptables -A $CHAIN_NAME -s 10.0.1.1/24 -i wg1 -d 10.2.0.100 -o eth0 -p udp --dport 53 -j ACCEPT
+
+iptables -A $CHAIN_NAME -s 10.0.1.1/24 -i wg1 -d 10.2.0.3 -p udp --dport 771 -j ACCEPT
+iptables -A $CHAIN_NAME -s 10.2.0.3 -i wg1 -d 10.0.1.1/24 -p udp --dport 771 -j ACCEPT
+
+iptables -A $CHAIN_NAME -s 10.0.1.1/24 -o wg1 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# Drop traffic to your any private IP address
+iptables -A $CHAIN_NAME -s 10.0.1.1/24 -i wg1 -d 10.2.0.3 -p tcp -m multiport --dports 22,8080 -j DROP
+# Accept outgoing connections to HTTP(S) ports to any IP address (public because of rule above)
+iptables -A $CHAIN_NAME -s 10.0.1.1/24 -i wg1 -d 10.2.0.3 -p tcp -m multiport --dports 80,443 -j ACCEPT
+iptables -A $CHAIN_NAME -s 10.2.0.3 -i wg1 -d 10.0.1.1/24 -p tcp --dports 80,443 -j ACCEPT
 # Return to FORWARD chain
 iptables -A $CHAIN_NAME -j RETURN
