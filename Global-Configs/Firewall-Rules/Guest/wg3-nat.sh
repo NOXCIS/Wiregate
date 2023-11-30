64df30c995ef4af6270f3af99d98386989780085
NOXCIS <shamarakeillee@gmail.com>
2023-11-30 03:12:21 -0600

    +

--- Global-Configs/Firewall-Rules/Guest/wg3-nat.sh
+++ Global-Configs/Firewall-Rules/Guest/wg3-nat.sh
@@ -15,19 +15,12 @@ iptables -A FORWARD -j $CHAIN_NAME
 # Accept related or established traffic
 iptables -A $CHAIN_NAME -o $WIREGUARD_INTERFACE -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
-# Accept traffic from any Wireguard IP address connected to the Wireguard server
-#iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -j ACCEPT
-
-
 # DNS
 iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 10.2.0.100 -p udp --dport 53 -j ACCEPT
 # Drop traffic to your any private IP address
 iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 -j DROP
 # Accept outgoing connections to HTTP(S) ports to any IP address (public because of rule above)
 iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 0.0.0.0/0 -p tcp -m multiport --dports 80,443 -j ACCEPT
-#Change to Drop traffic from WIREGUARD_LAN to 10.2.0.100:80 on internet
-#iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 10.0.0.0/8,172.16.0.0/12 -j DROP
-
 
 # Drop everything else coming through the Wireguard interface
 iptables -A $CHAIN_NAME -i $WIREGUARD_INTERFACE -j DROP
--- Global-Configs/Firewall-Rules/Members/wg1-nat.sh
+++ Global-Configs/Firewall-Rules/Members/wg1-nat.sh
@@ -13,18 +13,16 @@ iptables -A FORWARD -j $CHAIN_NAME
 # Accept related or established traffic
 iptables -A $CHAIN_NAME -o $WIREGUARD_INTERFACE -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
-# Accept traffic from any Wireguard IP address connected to the Wireguard server
-#iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -j ACCEPT
-
+# Drop incoming traffic from wg1 to wg-dashboard
+iptables -A INPUT -i wg1 -j DROP
 
 # DNS
 iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 10.2.0.100 -p udp --dport 53 -j ACCEPT
-# Drop traffic to your any private IP address
-iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 10.0.0.0/8,172.16.0.0/12 -j DROP
+
 # Accept outgoing connections to HTTP(S) ports to any IP address (public because of rule above)
 iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 0.0.0.0/0 -p tcp -m multiport --dports 80,443 -j ACCEPT
-#Change to Drop traffic from WIREGUARD_LAN to 10.2.0.100:80 on internet
-#iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 10.0.0.0/8,172.16.0.0/12 -j DROP
+
+
 
 
 # Drop everything else coming through the Wireguard interface
