a436dacc47a5cb3821c89b264439e0c0a46204c6
NOXCIS <shamarakeillee@gmail.com>
2023-11-30 02:35:22 -0600

    Update wg1-nat.sh

--- Global-Configs/Firewall-Rules/Members/wg1-nat.sh
+++ Global-Configs/Firewall-Rules/Members/wg1-nat.sh
@@ -20,11 +20,11 @@ iptables -A $CHAIN_NAME -o $WIREGUARD_INTERFACE -m conntrack --ctstate RELATED,E
 # DNS
 iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 10.2.0.100 -p udp --dport 53 -j ACCEPT
 # Drop traffic to your any private IP address
-iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 192.168.10.1/24 -j DROP
+iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 10.0.0.0/8,172.16.0.0/12 -j DROP
 # Accept outgoing connections to HTTP(S) ports to any IP address (public because of rule above)
 iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 0.0.0.0/0 -p tcp -m multiport --dports 80,443 -j ACCEPT
-
-
+#Change to Drop traffic from WIREGUARD_LAN to 10.2.0.100:80 on internet
+#iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -d 10.0.0.0/8,172.16.0.0/12 -j DROP
 
 
 # Drop everything else coming through the Wireguard interface
