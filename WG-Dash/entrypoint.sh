af238ea84f1650bc6fc66ebaf960a3e64ff49da0
Noxcis <shamarakeillee@gmail.com>
2023-12-02 16:42:53 -0800

    dos2unix fix
    man i hate windows

--- WG-Dash/entrypoint.sh
+++ WG-Dash/entrypoint.sh
@@ -19,16 +19,13 @@ if [ ! -f "/etc/wireguard/wg0.conf" ]; then
     /home/app/wgd.sh newconfig
 
 fi
-
 run_wireguard_up() {
   config_files=$(find /etc/wireguard -type f -name "*.conf")
 
   for file in $config_files; do
     config_name=$(basename "$file" ".conf")
     chmod 600 "/etc/wireguard/$config_name.conf"
-    wg-quick up "$config_name" 
-   
-   
+    wg-quick up "$config_name"  
   done
   
 }
