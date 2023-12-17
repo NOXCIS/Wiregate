89da98a84dd19770a6b7a88beaf0677203f8d7fc
Noxcis <shamarakeillee@gmail.com>
2023-12-16 16:02:31 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -206,7 +206,7 @@ pihole_predefined_setup () {
         if [ ! -f "$install_check" ]; then
             # If prerequisites are not installed, install them
             install_requirements
-            docker compose down  --remove-orphans
+            docker-compose down --remove-orphans && docker volume rm $(docker volume ls -qf name=wg_data)
         elif [ -f "$install_check" ]; then
             # If prerequisites are installed, bring down the Docker-compose setup
             docker compose down  --remove-orphans
@@ -266,32 +266,7 @@ pihole_predefined_setup () {
         history -c
         clear
     }
-    create_swap() {
 
-        # Check if a swapfile already exists
-        if [[ -f /swapfile ]]; then
-            echo "Swapfile already exists."
-            exit 1
-        fi
-
-        # Create a swapfile
-            sudo fallocate -l 2G /swapfile
-
-        # Set permissions for the swapfile
-            sudo chmod 600 /swapfile
-
-        # Set up the swap space
-            sudo mkswap /swapfile
-
-        # Enable the swapfile
-            sudo swapon /swapfile
-
-        # Update the fstab file to make the swapfile persistent across reboots
-            echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-            echo "Swapfile created and enabled."
-
-
-}
 
 
 # Main script
