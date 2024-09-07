42b74ef249bf5a9cf8d91e88955d1a535e4a03a3
Noxcis <shamarakeillee@gmail.com>
2024-09-07 02:30:46 -0500

    it works

--- WG-Dash/src/iptable-rules/Admins/postup.sh
+++ WG-Dash/src/iptable-rules/Admins/postup.sh
@@ -2,25 +2,67 @@
 WIREGUARD_INTERFACE=ADMINS
 WIREGUARD_LAN=10.0.0.1/24
 MASQUERADE_INTERFACE=eth0
+DNS_SERVER=10.2.0.3
+DNS_UDP_PORT=53530
+PROXY_PORT=59040
+
+# Accept established incoming connections
+iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT
+
+# Allow local loopback traffic
+#iptables -t nat -A INPUT -i lo -j ACCEPT
+#iptables -A OUTPUT -d 10.2.0.3/32 -o lo -j ACCEPT
+
+
+# Allow local loopback traffic
+iptables -A INPUT -i lo -s $WIREGUARD_LAN -j ACCEPT
+iptables -A OUTPUT -o lo -s $WIREGUARD_LAN -j ACCEPT
+
+# Allow new incoming traffic on WireGuard interface
+iptables -A INPUT -i $WIREGUARD_INTERFACE -s $WIREGUARD_LAN -m state --state NEW -j ACCEPT
 
 iptables -t nat -I POSTROUTING -o $MASQUERADE_INTERFACE -j MASQUERADE -s $WIREGUARD_LAN
 
-# Add a WIREGUARD_wg0 chain to the FORWARD chain
-CHAIN_NAME="WIREGUARD_$WIREGUARD_INTERFACE"
-iptables -N $CHAIN_NAME
-iptables -A FORWARD -j $CHAIN_NAME
 
-# Accept related or established traffic
-iptables -A $CHAIN_NAME -o $WIREGUARD_INTERFACE -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+# DNS Redirection for UDP traffic (Port 53)
+iptables -t nat -A PREROUTING -i $WIREGUARD_INTERFACE -p udp --dport 53 -j DNAT --to-destination 10.2.0.100:53
 
-# Accept traffic from any Wireguard IP address connected to the Wireguard server
-iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -j ACCEPT
+# Exclude private address ranges from proxy
+for NET in 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16; do
+  iptables -t nat -A PREROUTING -i $WIREGUARD_INTERFACE -d $NET -j RETURN
+done
+iptables -t nat -A PREROUTING -i $WIREGUARD_INTERFACE -d $MASQUERADE_INTERFACE -j RETURN
 
-# Allow traffic to the local loopback interface
-iptables -A $CHAIN_NAME -o lo -j ACCEPT
+# Redirect remaining TCP traffic to the proxy
+iptables -t nat -A PREROUTING -i $WIREGUARD_INTERFACE -p tcp -j DNAT --to-destination $DNS_SERVER:$PROXY_PORT
 
-# Drop everything else coming through the Wireguard interface
-iptables -A $CHAIN_NAME -i $WIREGUARD_INTERFACE -j DROP
+# Redirect remaining UDP traffic to the proxy
+iptables -t nat -A PREROUTING -i $WIREGUARD_INTERFACE -p udp -j DNAT --to-destination $DNS_SERVER:$PROXY_PORT
 
-# Return to FORWARD chain
-iptables -A $CHAIN_NAME -j RETURN
+# Allow new outgoing TCP connections
+iptables -A OUTPUT -o $MASQUERADE_INTERFACE -p tcp --tcp-flags FIN,SYN,RST,ACK SYN -m state --state NEW -j ACCEPT
+
+
+# Redirect specific network ranges to the proxy
+iptables -t nat -A OUTPUT -d 10.192.0.0/10 -p tcp -m tcp -j DNAT --to-destination $DNS_SERVER:$PROXY_PORT
+
+# Accept established outgoing traffic
+iptables -A OUTPUT -m state --state ESTABLISHED -j ACCEPT
+
+# Drop invalid packets
+iptables -A OUTPUT -m conntrack --ctstate INVALID -j DROP
+iptables -A OUTPUT -m state --state INVALID -j DROP
+
+# Exclude private network ranges from NAT
+for NET in 127.0.0.0/8 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16 0.0.0.0/8 100.64.0.0/10 169.254.0.0/16 192.0.0.0/24 \
+           192.0.2.0/24 192.88.99.0/24 198.18.0.0/15 198.51.100.0/24 203.0.113.0/24 224.0.0.0/4 240.0.0.0/4 \
+           255.255.255.255/32; do
+  iptables -t nat -A OUTPUT -d $NET -j RETURN
+done
+
+# Allow loopback traffic to bypass NAT
+iptables -t nat -A OUTPUT -o lo -j RETURN
+
+# Drop invalid TCP FIN/ACK and RST/ACK packets not from loopback
+iptables -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp --tcp-flags ACK,FIN ACK,FIN -j DROP
+iptables -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp --tcp-flags ACK,RST ACK,RST -j DROP
