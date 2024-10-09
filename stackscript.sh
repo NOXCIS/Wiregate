d7692a9ef44efa6130b8d6567e01de84e2d1fa72
Noxcis <shamarakeillee@gmail.com>
2024-10-08 23:44:19 -0500

    Update stackscript.sh

--- stackscript.sh
+++ stackscript.sh
@@ -8,14 +8,14 @@ ARG3=""
 ARG4=""
 
 # Parse options
-while getopts "b:r:t:n:dd:" opt; do
+while getopts "b:r:t:n:d:" opt; do
   case "$opt" in
     b)  BRANCH="$OPTARG" ;;   # -b for branch
     r)  ARG1="$OPTARG" ;;     # -1 for ARG1
     t)  ARG2="$OPTARG" ;;     # -2 for ARG2
     n)  ARG3="$OPTARG" ;;     # -3 for ARG3
-   dd)  ARG4="$OPTARG" ;;     # -4 for ARG4
-    \?) echo "Usage: $0 [-b branch] [-r arg1] [-t arg2] [-n arg3] [-dd arg4]"; exit 1 ;;
+    d)  ARG4="$OPTARG" ;;     # -4 for ARG4
+    \?) echo "Usage: $0 [-b branch] [-r arg1] [-t arg2] [-n arg3] [-d arg4]"; exit 1 ;;
   esac
 done
 shift "$((OPTIND - 1))"  # Shift past processed options
