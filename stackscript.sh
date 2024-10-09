605f4a2fbdb29d304238023705a02f2f177547b5
Noxcis <shamarakeillee@gmail.com>
2024-10-08 23:28:09 -0500

    Update stackscript.sh

--- stackscript.sh
+++ stackscript.sh
@@ -1,49 +1,52 @@
 #!/bin/bash
 
-BRANCH=${1:-main}
-ARG1=${2:-}
-ARG2=${3:-}
-ARG3=${4:-}
-ARG4=${5:-}
-DEV_BRANCH="main-patch"
+# Initialize variables for flags
+BRANCH="main"
+ARG1=""
+ARG2=""
+ARG3=""
+ARG4=""
+
+# Parse options
+while getopts "b:r:t:n:dd:" opt; do
+  case "$opt" in
+    b)  BRANCH="$OPTARG" ;;   # -b for branch
+    r)  ARG1="$OPTARG" ;;     # -1 for ARG1
+    t)  ARG2="$OPTARG" ;;     # -2 for ARG2
+    n)  ARG3="$OPTARG" ;;     # -3 for ARG3
+   dd)  ARG4="$OPTARG" ;;     # -4 for ARG4
+    \?) echo "Usage: $0 [-b branch] [-r arg1] [-t arg2] [-n arg3] [-dd arg4]"; exit 1 ;;
+  esac
+done
+shift "$((OPTIND - 1))"  # Shift past processed options
 
 
-if [ "$BRANCH" = "dind" ]; then
-    ARG4="$BRANCH"
-    BRANCH="$DEV_BRANCH"
-fi
-if [ "$BRANCH" = "dev" ] || \
-   [ "$BRANCH" = "help" ] || \
-   [ "$BRANCH" = "reset" ]; then
-    ARG1="$BRANCH"
-    BRANCH="$DEV_BRANCH"
-fi
+
 
 
 git clone --branch $BRANCH https://github.com/NOXCIS/Wiregate.git   
 cd Wiregate
- 
+
 
 if [ "$ARG4" = "dind" ]; then
-
-    env_file=".env"
+        env_file=".env"
 
     if [ ! -f "$env_file" ]; then
         touch "$env_file"
-    fi
+        fi
 
     if [ ! -s "$env_file" ]; then 
         ip=$(curl -s ifconfig.me)
-        echo "WGD_PORT_RANGE_STARTPORT=\"443\"" >> "$env_file"
-        echo "WGD_PORT_MAPPINGS=\"443-448:443-448/udp\"" >> "$env_file"
-        echo "WGD_REMOTE_ENDPOINT=\"$ip\"" >> "$env_file"
-    fi
+            echo "WGD_PORT_RANGE_STARTPORT=\"443\"" >> "$env_file"
+            echo "WGD_PORT_MAPPINGS=\"443-448:443-448/udp\"" >> "$env_file"
+            echo "WGD_REMOTE_ENDPOINT=\"$ip\"" >> "$env_file"
+        fi
 fi
-
 
 chmod +x install.sh
 
-# Only pass ARG1, ARG2, and ARG3 to install.sh
+
+
 if [[ -n "$ARG1" && -n "$ARG2" && -n "$ARG3" ]]; then
     ./install.sh "$ARG1" "$ARG2" "$ARG3"
 
