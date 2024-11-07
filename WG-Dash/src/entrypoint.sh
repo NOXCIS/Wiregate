962a0372ebfcbff77868f0974c70a31be5460b2e
Noxcis <shamarakeillee@gmail.com>
2024-11-07 03:35:23 -0500

    optimizations

--- WG-Dash/src/entrypoint.sh
+++ WG-Dash/src/entrypoint.sh
@@ -90,13 +90,9 @@ make_torrc() {
     echo -e "User tor \n" >> "$TORRC_PATH"
     echo -e "DataDirectory /var/lib/tor \n" >> "$TORRC_PATH"
     echo -e "TransPort ${INET_ADDR}:59040 IsolateClientAddr IsolateClientProtocol IsolateDestAddr IsolateDestPort \n" >> "$TORRC_PATH"
-    if [[ "$WGD_TOR_PLUGIN" == "obfs4" ]]; then
-    echo -e "ClientTransportPlugin obfs4 exec /usr/local/bin/lyrebird \n" >> "$TORRC_PATH"
-    elif [[ "$WGD_TOR_PLUGIN" == "snowflake" ]]; then
-    echo -e "ClientTransportPlugin snowflake exec /usr/local/bin/snowflake -url https://snowflake-broker.azureedge.net/ -front ajax.aspnetcdn.com -ice stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn \n" >> "$TORRC_PATH"
-    else
+
     echo -e "ClientTransportPlugin ${WGD_TOR_PLUGIN} exec /usr/local/bin/${WGD_TOR_PLUGIN} \n" >> "$TORRC_PATH"
-    fi
+    
     if [[ "$WGD_TOR_EXIT_NODES" == "default" ]]; then
     echo "Using Default"
     elif [[ -n "$WGD_TOR_EXIT_NODES" ]]; then
@@ -126,12 +122,25 @@ make_dns_torrc() {
     if [ -f "$DNS_TORRC_PATH" ]; then
     rm "$DNS_TORRC_PATH" 
     fi
+
+    if [[ "$WGD_TOR_PLUGIN" == "webtunnel" ]]; then
+    get_webtunnel_bridges
+    elif [[ "$WGD_TOR_PLUGIN" == "obfs4" ]]; then
+    get_obfs4_bridges
+    fi
+
+    if [[ "$WGD_TOR_BRIDGES" == "true" ]]; then
     echo -e "UseBridges 1\n" >> "$DNS_TORRC_PATH"
+    printf "[TOR] Adding bridges to $DNS_TORRC_PATH...\n"
+    printf "[TOR] Bridges added to $DNS_TORRC_PATH successfully!\n"
+    fi
+
     echo -e "AutomapHostsOnResolve 1 \n" >> "$DNS_TORRC_PATH"
     echo -e "VirtualAddrNetwork 10.193.0.0/10 \n" >> "$TORRC_PATH"
     echo -e "User tor \n" >> "$DNS_TORRC_PATH"
     echo -e "DataDirectory /var/lib/tor/dns \n" >> "$DNS_TORRC_PATH"
-    echo -e "ClientTransportPlugin snowflake exec /usr/local/bin/snowflake  \n" >> "$DNS_TORRC_PATH"
+    echo -e "ClientTransportPlugin ${WGD_TOR_PLUGIN} exec /usr/local/bin/${WGD_TOR_PLUGIN} \n" >> "$DNS_TORRC_PATH"
+    #echo -e "ClientTransportPlugin snowflake exec /usr/local/bin/snowflake  \n" >> "$DNS_TORRC_PATH"
     if [[ "$WGD_TOR_DNS_EXIT_NODES" == "default" ]]; then
     echo "Using Default"
     elif [[ -n "$WGD_TOR_DNS_EXIT_NODES" ]]; then
@@ -141,8 +150,21 @@ make_dns_torrc() {
     fi
 
     echo -e "SocksPort ${INET_ADDR}:9053 \n" >> "$DNS_TORRC_PATH"
+
+    if [[ "$WGD_TOR_PLUGIN" == "snowflake" ]]; then
     echo -e "Bridge snowflake 192.0.2.3:80 2B280B23E1107BB62ABFC40DDCC8824814F80A72 fingerprint=2B280B23E1107BB62ABFC40DDCC8824814F80A72 url=https://1098762253.rsc.cdn77.org/ fronts=www.cdn77.com,www.phpmyadmin.net ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn \n" >> "$DNS_TORRC_PATH"
     echo -e "Bridge snowflake 192.0.2.4:80 8838024498816A039FCBBAB14E6F40A0843051FA fingerprint=8838024498816A039FCBBAB14E6F40A0843051FA url=https://1098762253.rsc.cdn77.org/ fronts=www.cdn77.com,www.phpmyadmin.net ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.net:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn \n" >> "$DNS_TORRC_PATH"
+  
+
+    
+    else
+    echo "$bridges" | while read -r bridge; do
+        echo "Bridge $bridge" >> "$DNS_TORRC_PATH"
+    done
+    fi
+
+    #echo -e "Bridge snowflake 192.0.2.3:80 2B280B23E1107BB62ABFC40DDCC8824814F80A72 fingerprint=2B280B23E1107BB62ABFC40DDCC8824814F80A72 url=https://1098762253.rsc.cdn77.org/ fronts=www.cdn77.com,www.phpmyadmin.net ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn \n" >> "$DNS_TORRC_PATH"
+    #echo -e "Bridge snowflake 192.0.2.4:80 8838024498816A039FCBBAB14E6F40A0843051FA fingerprint=8838024498816A039FCBBAB14E6F40A0843051FA url=https://1098762253.rsc.cdn77.org/ fronts=www.cdn77.com,www.phpmyadmin.net ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.net:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn \n" >> "$DNS_TORRC_PATH"
    
     printf "%s\n" "$dashes"
 }
