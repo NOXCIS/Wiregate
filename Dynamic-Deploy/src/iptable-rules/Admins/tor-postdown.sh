8f1907382242453fa8ebdc561cf185da9632ea4e
Noxcis <shamarakeillee@gmail.com>
2024-11-23 01:06:53 -0500

    +

--- Dynamic-Deploy/src/iptable-rules/Admins/tor-postdown.sh
+++ Dynamic-Deploy/src/iptable-rules/Admins/tor-postdown.sh
@@ -19,22 +19,3 @@ iptables -X ADMIN_RULES
 iptables -t nat -X ADMIN_RULES
 
 
-# Remove WireGuard LAN traffic masquerading
-iptables -t nat -D POSTROUTING -o $MASQUERADE_INTERFACE -j MASQUERADE -s $WIREGUARD_LAN
-
-# Remove DNS redirection for UDP traffic
-iptables -t nat -D PREROUTING -i $WIREGUARD_INTERFACE -p udp --dport 53 -j DNAT --to-destination $DNS_SERVER:$DNS_UDP_PORT
-
-# Remove exclusions for private address ranges from proxy
-for NET in 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16; do
-  iptables -t nat -D PREROUTING -i $WIREGUARD_INTERFACE -d $NET -j RETURN
-done
-
-# Remove prevention of masqueraded interface traffic redirection
-iptables -t nat -D PREROUTING -i $WIREGUARD_INTERFACE -d $MASQUERADE_INTERFACE -j RETURN
-
-# Remove redirections of remaining TCP and UDP traffic to the proxy
-iptables -t nat -D PREROUTING -i $WIREGUARD_INTERFACE -p tcp -j DNAT --to-destination $PROXY:$PROXY_PORT
-iptables -t nat -D PREROUTING -i $WIREGUARD_INTERFACE -p udp -j DNAT --to-destination $PROXY:$PROXY_PORT
-
-
--- Dynamic-Deploy/src/iptable-rules/Members/tor-postdown.sh
+++ Dynamic-Deploy/src/iptable-rules/Members/tor-postdown.sh
@@ -18,15 +18,3 @@ iptables -t nat -F MEMBERS_RULES
 iptables -X MEMBERS_RULES
 iptables -t nat -X MEMBERS_RULES
 
-
-
-iptables -t nat -D POSTROUTING -o $MASQUERADE_INTERFACE -d 10.2.0.100 -p udp -m multiport --dports 53,853 -j MASQUERADE -s $WIREGUARD_LAN
-iptables -t nat -D POSTROUTING -o $MASQUERADE_INTERFACE -d 10.2.0.100 -p tcp -m multiport --dports 53,853 -j MASQUERADE -s $WIREGUARD_LAN
-
-# DNS Redirection for UDP traffic (Port 53) [NEEDED]
-iptables -t nat -D PREROUTING -i $WIREGUARD_INTERFACE -p udp -m multiport --dports 53,853 -j DNAT --to-destination $DNS_SERVER:$DNS_UDP_PORT
-iptables -t nat -D PREROUTING -i $WIREGUARD_INTERFACE -d 10.2.0.4 -j RETURN
-iptables -t nat -D PREROUTING -i $WIREGUARD_INTERFACE -d $MASQUERADE_INTERFACE -j RETURN
-iptables -t nat -D PREROUTING -i $WIREGUARD_INTERFACE -p tcp -j DNAT --to-destination $PROXY:$PROXY_PORT
-iptables -t nat -D PREROUTING -i $WIREGUARD_INTERFACE -p udp -j DNAT --to-destination $PROXY:$PROXY_PORT
-
